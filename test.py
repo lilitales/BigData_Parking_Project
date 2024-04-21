170a5f34365e8bead6214f638153695cd2ddab28
lilitales <snowfairytaka@gmail.com>
2024-04-21 22:52:15 +0800

    撰寫程式: 從MongoDB根據條件篩選和取特定欄位， 畫出指定停車場的剩餘停車位折線圖

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import Mongodb
 from data import DataAccess
 from auth import Auth
 import json
@@ -25,17 +26,41 @@ auth = Auth()
 # print(res.total_seconds() / A3600)
 
 
-parkingAvail_url = r"https://tdx.transportdata.tw/api/basic/v1/Parking/OffStreet/ParkingAvailability/City/{city}".format(
-    city='aaa')
+# parkingAvail_url = r"https://tdx.transportdata.tw/api/basic/v1/Parking/OffStreet/ParkingAvailability/City/{city}".format(
+#     city='aaa')
 
-paramStr = data._build_query_params()
-send_url = parkingAvail_url + '?' + paramStr
-try:
-    response = data._send_request(send_url)
-    print(response)
-except Exception as e:
-    print(e)
+# paramStr = data._build_query_params()
+# send_url = parkingAvail_url + '?' + paramStr
+# try:
+#     response = data._send_request(send_url)
+#     print(response)
+# except Exception as e:
+#     print(e)
 
 
 # p = Path(f'./log/123_errLog.txt')
 # print(p.resolve())
+
+
+# db = Mongodb.obtain_db()
+# park = db['Time_ParkingAvailability']
+
+# # query_date = "2024-04-15"
+# # query = {"UpdateTime": {"$regex": f"^{query_date}"}}
+# query = {
+#     "ParkingAvailabilities": {
+#         "$elemMatch": {
+#             "CarParkName.Zh_tw": "台南成大醫院復健大樓停車場"
+#         }
+#     }
+# }
+
+# print(park.estimated_document_count())
+# cursor = park.find(query).limit(2)
+# print(len(list(cursor)))
+# print('done')
+
+# 測試日期取時分秒
+dateFormat = r"%Y-%m-%dT%H:%M:%S%z"
+date_test = datetime.strptime("2024-04-20T14:43:01+08:00", dateFormat)
+print(datetime.strftime(date_test, "%H:%M"))
